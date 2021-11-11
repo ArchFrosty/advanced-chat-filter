4a0ad9fd25a0f0e86194bc54e82ef35ec6d506ee
Igor <igorveres00@gmail.com>
2021-11-11 18:08:53 +0100

    yes

---
+++ src/main/java/chatfilter/AdvancedChatFilterType.java
@@ -0,0 +1,8 @@
+package chatfilter;
+
+public enum AdvancedChatFilterType
+{
+    CENSOR_WORDS,
+    CENSOR_MESSAGE,
+    REMOVE_MESSAGE
+}
--- src/test/java/chatfilter/ExamplePluginTest.java
+++ src/test/java/chatfilter/ExamplePluginTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package chatfilter;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
